0a74dcc64f896abac1f97868a0a961a9cc304d10
Leticia Molina <letimolina@gmail.com>
2018-06-10 20:19:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,22 @@
 class Post
-  attr_accessor :title, :artist
+  attr_accessor :name, :artist
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def artist_name
+    if self.artist == nil
+      return nil
+    else
+      self.artist.name
+    end
+  end
+  
+end
 end
