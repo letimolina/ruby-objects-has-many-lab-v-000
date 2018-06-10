7ad36be7c371736393f2a7f01aa9d6587fdfbc0d
Leticia Molina <letimolina@gmail.com>
2018-06-10 20:09:57 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,30 +11,26 @@ class Artist
   
   def add_song(song)
     @songs << song
-   describe "#add_song" do
-      it "takes in an argument of a song and associates that song with the artist by telling the song that it belongs to that artist" do
-        hello = Song.new("Hello")
-        adele.add_song(hello)
-        expect(adele.songs).to include(hello)
-        expect(hello.artist).to eq(adele)
-      end
-    end
-
-    describe "#add_song_by_name" do
-      it "takes in an argument of a song name, creates a new song with it and associates the song and artist" do
-        adele.add_song_by_name("Rolling in the Deep")
-        expect(adele.songs.last.name).to eq("Rolling in the Deep")
-        expect(adele.songs.last.artist).to eq(adele)
-      end
-    end
-
-    describe ".song_count" do
-      it "is a class method that returns the total number of songs associated to all existing artists" do
-        expect(Artist.song_count).to eq(2)
-      end
-    end
-    
+    all_songs << song
+    song.artist = self
   end
-end
+  
+
+#     describe "#add_song_by_name" do
+#       it "takes in an argument of a song name, creates a new song with it and associates the song and artist" do
+#         adele.add_song_by_name("Rolling in the Deep")
+#         expect(adele.songs.last.name).to eq("Rolling in the Deep")
+#         expect(adele.songs.last.artist).to eq(adele)
+#       end
+#     end
+
+#     describe ".song_count" do
+#       it "is a class method that returns the total number of songs associated to all existing artists" do
+#         expect(Artist.song_count).to eq(2)
+#       end
+#     end
+    
+#   end
+# end
 
 end
